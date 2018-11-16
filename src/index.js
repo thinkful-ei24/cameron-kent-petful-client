f30c5798c00e958736c0abdfea363d486097563e
Kent Tokunaga <kenttokunaga@gmail.com>
2018-11-16 11:06:14 -0500

    Render the react component

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 import './index.css';
-import App from './App';
+import Dashboard from './Dashboard';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Dashboard />
   </Provider>,
   document.getElementById('root')
 );
