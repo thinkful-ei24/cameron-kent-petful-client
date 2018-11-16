d66cd10ef30807e944b9a7426c23c7f0c03ea9ff
Cameron Hatch <cameronlavigne92@gmail.com>
2018-11-16 09:37:49 -0700

    added actions

---
+++ src/actions/dog.js
@@ -0,0 +1,48 @@
+import { API_BASE_URL } from '../config';
+import { normalizeResponseErrors } from './utils';
+
+
+
+export const FETCH_DOG_REQUEST = 'FETCH_DOG_REQUEST';
+export const fetchDogRequest = () => {
+  return ({
+    type: FETCH_DOG_REQUEST
+  }) ; 
+};
+
+export const FETCH_DOG_SUCCESS= 'FETCH_DOG_SUCCESS';
+export const fetchDogSuccess= (dogObj) => {
+  return ({
+    type: FETCH_DOG_SUCCESS,
+    dog: dogObj
+  }) ; 
+};
+
+export const FETCH_DOG_ERROR= 'FETCH_DOG_ERROR';
+export const fetchDogError= (err) => {
+  return ({
+    type: FETCH_DOG_ERROR,
+    error: err
+  }) ; 
+};
+
+
+export const fetchDog = () => dispatch => {
+  dispatch(fetchDogRequest());
+  return fetch(`${API_BASE_URL}/api/dog`)
+  .then(res => normalizeResponseErrors(res))
+  .then(res => res.json())
+  .then((res) => dispatch(fetchDogSuccess(res)))
+  .catch(err => dispatch(fetchDogError(err))); 
+}
+
+export const deleteDog = () => dispatch => {
+  dispatch(fetchDogRequest());
+  return fetch(`${API_BASE_URL}/api/dog`, {
+    method: 'DELETE'
+  })
+  .then(res => normalizeResponseErrors(res))
+  .then(res => res.json())
+  .then(() => dispatch(fetchDogRequest()))
+  .catch(err => dispatch(fetchDogError(err))); 
+}
